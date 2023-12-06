50d3e0739b82a5853d12fdf2fdeac6b122937f95
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-12-06 15:55:14 +0300

    no message

--- app/src/main/java/com/example/unite3/MainActivity.java
+++ app/src/main/java/com/example/unite3/MainActivity.java
@@ -50,5 +50,24 @@ public class MainActivity extends AppCompatActivity {
     public void uyg7(View view) {
         Intent i = new Intent(MainActivity.this, Uyg7.class);
         startActivity(i);
+
+    }
+
+    public void uyg8(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg8.class);
+        startActivity(i);
+    }
+
+    public void uyg9(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg9.class);
+        startActivity(i);
+    }
+    public void uyg10(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg10.class);
+        startActivity(i);
+    }
+    public void uyg11(View view) {
+        Intent i = new Intent(MainActivity.this, Uyg11.class);
+        startActivity(i);
     }
 }
